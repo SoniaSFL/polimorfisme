e751bb507938444e594576f8da53f3456b937411
Sonia Sanchez <soniasanchezfl@gmail.com>
2019-07-21 20:59:56 +0200

    First commit

---
+++ JobsProject/src/com/jobs/domain/Volunteer.java
@@ -0,0 +1,21 @@
+package com.jobs.domain;
+
+public class Volunteer extends AbsStaffMember {
+
+	public Volunteer(String name, String address, String phone) throws Exception {
+		super(name, address, phone);
+		
+	}
+	
+	
+	@Override
+	public void pay() {
+		totalPaid = 0;
+	}
+
+	@Override
+	public String toString() {
+		return "Volunteer [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + ", totalPaid=" + totalPaid + "]";
+	}
+	
+}
